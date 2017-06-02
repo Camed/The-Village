681731c5abf6bdfb6f8f5c868b0f5e5e96540d11
camed_ <chomiczewskigabriel@gmail.com>
2017-06-02 19:28:32 +0200

    #1 commit
    first commit

---
+++ The Village/Assets/GUI/Buttons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public static class Buttons
+{ 
+
+    public static void NewGame()
+    {
+        Game.Start();
+    }
+
+    public static void LoadLastGame()
+    {
+        string[] save = File.ReadAllLines(@"./saves/save.txt");
+
+        int wood = 0, stone = 0, money = 0, water = 0, food = 0, tartak = 0, kamieniolom = 0, radaosady = 0, zrodlo = 0, farma = 0,
+            lvl = 0, attack = 0, defense = 0, mp = 0, mr = 0;
+
+        int.TryParse(save[0],  out wood);        int.TryParse(save[1],  out stone);
+        int.TryParse(save[2],  out money);       int.TryParse(save[3],  out water);
+        int.TryParse(save[4],  out food);        int.TryParse(save[5],  out tartak);
+        int.TryParse(save[6],  out kamieniolom); int.TryParse(save[7],  out radaosady);
+        int.TryParse(save[8],  out zrodlo);      int.TryParse(save[9],  out farma);
+        int.TryParse(save[10], out lvl);         int.TryParse(save[11], out attack);
+        int.TryParse(save[12], out defense);     int.TryParse(save[13], out mp);
+        int.TryParse(save[14], out mr);
+
+        Game.Load(wood, stone, money, water, food, tartak, kamieniolom, radaosady, zrodlo, farma, level, attack, defense, mp, mr);
+    }
+
+}
---
+++ The Village/Assets/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class Game
+{ 
+	public static void Start()
+    {
+	    	
+	}
+    
+    public static void Load(int wood, int stone, int money, int water, int food, int tartak,
+                            int kamieniolom, int radaosady, int zrodlo, int farma, int level,
+                            int attack, int defense, int mp, int mr)
+    {
+        
+    }
+}
