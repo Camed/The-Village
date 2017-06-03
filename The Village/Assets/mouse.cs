fb955e7e5f4b98ecce02a22075abd11ab2796b9e
ketrab100 <bartis300@gmail.com>
2017-06-03 21:41:17 +0200

    #3
    safsdaf

---
+++ The Village/Assets/mouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse : MonoBehaviour
+{
+    public int poziom;
+
+    void OnMouseOver()
+     {
+
+        // print(gameObject.name);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel(poziom);
+        }
+            
+    }
+}
